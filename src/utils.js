ba7714f26acd9c8334d63b4c3fbc8274c4c5e839
VikkyAblaeva <nifergirl@yandex.ru>
2022-11-09 23:10:17 +0300

    add buttons

--- src/utils.js
+++ src/utils.js
@@ -43,7 +43,7 @@ const getRss = (linkToFeed) => {
   .get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(linkToFeed)}`)
   .catch(() => { throw Error('networkError')})
 };
-
+//http://feeds.feedburner.com/Astrobene
 const parseRSS = (data) => {
   const parser = new DOMParser();
   const content = parser.parseFromString(data.data.contents, "text/xml");
@@ -65,26 +65,31 @@ const parseRSS = (data) => {
 
 const getPostsAndFeeds = (normalizeFeedPosts) => {
   const parentPosts = document.querySelector('#posts');
-  parentPosts.classList.add('border-end', 'border-primary');
+  parentPosts.classList.add('border-end', 'border-secondary', 'border-2');
   const p = document.querySelector('.display-6');
   p.textContent = 'Посты';
   const lead = document.querySelector('.lead');
   const feedTitle = document.querySelector('.h5');
   const feedDescription = document.querySelector('.text-muted');
-  lead.textContent = 'Фиды'; // сделать, чтобы добавлялось
-  feedTitle.textContent = normalizeFeedPosts.feed.title; // сделать, чтобы добавлялось
-  feedDescription.textContent = normalizeFeedPosts.feed.description; // сделать, чтобы добавлялось
+  lead.textContent = 'Фиды'; // сделать, чтобы добавлялось несколько
+  feedTitle.textContent = normalizeFeedPosts.feed.title; // сделать, чтобы добавлялось несколько
+  feedDescription.textContent = normalizeFeedPosts.feed.description; // сделать, чтобы добавлялось несколько
   const ul = document.createElement('ul');
   ul.classList.add('list-unstyled');
   normalizeFeedPosts.posts.map((post) => {
     const li = document.createElement('li');
+    li.classList.add('.d-block', 'm-2', 'fs-6', 'row');
     const a = document.createElement('a');
-    const strong = document.createElement('strong');
     a.setAttribute('href', post.link);
     a.setAttribute('target', '_blank');
+    a.classList.add('.d-block', 'col-10', 'bold-text');
     a.textContent = post.title;
-    strong.append(a);
-    li.append(strong);
+    const button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.classList.add('.d-block', 'btn', 'btn-outline-primary', 'col-2');
+    button.textContent = 'Просмотр';
+    li.append(a);
+    li.append(button);
     ul.append(li);
   });
   parentPosts.append(ul);
